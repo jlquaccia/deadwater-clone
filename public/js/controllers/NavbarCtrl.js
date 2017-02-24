5874d2266d9405199fd83f72045ccf17ff45af0b
Jason Quaccia <jlquaccia@gmail.com>
2017-02-23 21:49:04 -0800

    navbar fully responsive

--- public/js/controllers/NavbarCtrl.js
+++ public/js/controllers/NavbarCtrl.js
@@ -1,4 +1,6 @@
-angular.module('deadwater').controller('NavbarCtrl', [ function () {
+angular.module('deadwater').controller('NavbarCtrl', ['$rootScope', function ($rootScope) {
+  $rootScope.hamburgerOpen = false;
+
   // nav link hovers-------------------------------------------------------------------------------------------------------------------
     $('.navLinks a').mouseenter(function () {
       $(this).prev().css('width', '5px');
@@ -13,30 +15,65 @@ angular.module('deadwater').controller('NavbarCtrl', [ function () {
   // hamburger hover-------------------------------------------------------------------------------------------------------------------
     var width = $(window).width();
 
-    $('.container-lines').mouseenter(function () {
-      $('.navbarMenuWrapper').css({
-        'height': width >= 1050 ? '108px' : '158px',
-        'opacity': '1'
-      });
-
-      setTimeout(function () {
-        $('.navLinks li').css({
-          'opacity': '1',
-          'top': '0'
+    if (width >= 900) {
+      $('.container-lines').mouseenter(function () {
+        $('.navbarMenuWrapper').css({
+          'height': width >= 1050 ? '108px' : '158px',
+          'opacity': '1'
         });
-      }, 300);
-    });
 
-    $('.navbarMenuWrapper').on('mouseleave click', function () {
-      $('.navbarMenuWrapper').css({
-        'height': '0',
-        'opacity': '0'
+        setTimeout(function () {
+          $('.navLinks li').css({
+            'opacity': '1',
+            'top': '0'
+          });
+        }, 300);
       });
 
-      $('.navLinks li').css({
-        'top': '-10px',
-        'opacity': '0'
+      $('.navbarMenuWrapper').on('mouseleave click', function () {
+        $('.navbarMenuWrapper').css({
+          'height': '0',
+          'opacity': '0'
+        });
+
+        $('.navLinks li').css({
+          'top': '-10px',
+          'opacity': '0'
+        });
       });
-    });
+    }
   // end hamburger hover---------------------------------------------------------------------------------------------------------------
+  // click toggle hamburger -----------------------------------------------------------------------------------------------------------
+    $rootScope.toggleHamburger = function () {
+      if (width < 900) {
+        if (!$rootScope.hamburgerOpen) {
+          $rootScope.hamburgerOpen = true;
+
+          $('.navbarMenuWrapper').css({
+            'height': '158px',
+            'opacity': '1'
+          });
+
+          setTimeout(function () {
+            $('.navLinks li').css({
+              'opacity': '1',
+              'top': '0'
+            });
+          }, 300);
+        } else {
+          $rootScope.hamburgerOpen = false;
+
+          $('.navbarMenuWrapper').css({
+            'height': '0',
+            'opacity': '0'
+          });
+
+          $('.navLinks li').css({
+            'top': '-10px',
+            'opacity': '0'
+          });
+        }
+      }
+    };
+  // end click toggle hamburger -------------------------------------------------------------------------------------------------------
 }]);
