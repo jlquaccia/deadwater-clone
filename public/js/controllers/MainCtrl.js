3e0978e7e5cef6a6fbe09be1e093217859178a91
Jason Quaccia <jlquaccia@gmail.com>
2017-02-28 12:45:38 -0800

    implemented progress bar on loader

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,15 +1,35 @@
 angular.module('deadwater').controller('MainCtrl', ['$window', '$rootScope', function ($window, $rootScope) {
   $window.onload = function() {
-    setTimeout(function () {
-      $('.overlay-hugeinc.open').removeClass('open');
-    }, 2000);
+    progressBar();
   };
 
   $rootScope.$on('$stateChangeStart', function (e, toState, toParams, fromState, fromParams) {
     $('.overlay-hugeinc').addClass('open');
-
-    setTimeout(function () {
-      $('.overlay-hugeinc.open').removeClass('open');
-    }, 2000);
+    progressBar();
   });
+
+  function randomInteger (min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    
+    rand = Math.round(rand);
+    console.log('inside randomInteger');
+    return rand;
+  }
+  
+  var progressBar = function () {
+    var progress = document.querySelector('.js-progress');
+    var currentValue = progress.value;
+    var emulateProgress = setInterval(function () {
+      currentValue = currentValue + randomInteger(1, 10);
+      
+      if (currentValue >= 100) {
+        currentValue = 100;
+        clearInterval(emulateProgress);
+        $('.overlay-hugeinc.open').removeClass('open');
+        currentValue = 0;
+      }
+      
+      progress.value = currentValue;
+    }, 250);
+  };
 }]);
