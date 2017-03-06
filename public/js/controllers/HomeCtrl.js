674760bf91170a922a29ff36bdea3cebbe721255
Jason Quaccia <jlquaccia@gmail.com>
2017-03-05 21:39:36 -0800

    progress with waypoint animations

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -12,4 +12,31 @@ angular.module('deadwater').controller('HomeCtrl', ['$scope', function ($scope) 
       'left': '0'
     });
   };
+
+  // waypoints
+  $('.homeImgOneWrapper').waypoint(function () {
+    $('.homeImgOneWrapper').css('width', '100%');
+    $('.textWrapperOne .container-line').css({
+      'width': '20px',
+      'opacity': '1'
+    });
+    $('.textWrapperOne > span em, .textWrapperOne h1 span.one, .textWrapperOne h1 span.two').css({
+      'width': '100%',
+      'opacity': '1'
+    });
+  });
+
+  $('.textTwoOuter').waypoint(function () {
+    $('.textWrapperTwo .container-line').css({
+      'width': '20px',
+      'opacity': '1'
+    });
+
+    $('.textWrapperTwo > span em, .textWrapperTwo h1 span.one, .textWrapperTwo h1 span.two').css({
+      'width': '100%',
+      'opacity': '1'
+    });
+  }, {
+    offset: '75%'
+  });
 }]);
